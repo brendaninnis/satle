69f8ee268e96b9f7afa8962ce83e3ff7b73d951f
Brendan Innis <innisbrendan@gmail.com>
2022-08-01 10:51:46 -0700

    setup map styling and controls

--- satle/static/js/index.js
+++ satle/static/js/index.js
@@ -1,17 +1,22 @@
 // Initialize and add the map
 function initMap() {
-  // The location of Uluru
-  const uluru = { lat: -25.344, lng: 131.031 };
-  // The map, centered at Uluru
-  const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 4,
-    center: uluru,
-  });
-  // The marker, positioned at Uluru
-  const marker = new google.maps.Marker({
-    position: uluru,
-    map: map,
-  });
+    // The location of Uluru
+    const city = { lat: 48.4195002, lng: -123.3701672 };
+    // The map, centered at Uluru
+    const map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 18,
+        center: city,
+        disableDefaultUI: true,
+        gestureHandling: "none",
+        keyboardShortcuts: false,
+        zoomControl: false,
+        mapTypeControl: false,
+        scaleControl: false,
+        streetViewControl: false,
+        rotateControl: false,
+        fullscreenControl: false,
+        mapTypeId: google.maps.MapTypeId.SATELLITE
+    });
 }
 
 window.initMap = initMap;
